036fd7bbcc410da40f7c0d74f50f16f11dc483e7
Jonathan Reis <jonathanreis7834@gmail.com>
2019-03-29 13:50:21 -0300

    Novo banco de dados com adicao de algumas colunas em pedidos.

--- banco_dados/projeto.sql
+++ banco_dados/projeto.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: 29-Mar-2019 às 10:29
--- Versão do servidor: 5.7.21
--- PHP Version: 5.6.35
+-- Host: 127.0.0.1
+-- Generation Time: Mar 29, 2019 at 05:49 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,35 +25,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `pedido`
+-- Table structure for table `pedido`
 --
 
-DROP TABLE IF EXISTS `pedido`;
-CREATE TABLE IF NOT EXISTS `pedido` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `pedido` (
+  `id` int(11) NOT NULL,
   `id_cliente` int(11) NOT NULL,
   `id_produto` int(11) NOT NULL,
-  `data` date NOT NULL,
-  PRIMARY KEY (`id`)
+  `quantidade` int(10) NOT NULL,
+  `data` date NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `pedido`
+--
+
+INSERT INTO `pedido` (`id`, `id_cliente`, `id_produto`, `quantidade`, `data`) VALUES
+(5, 17, 1, 7, '2019-03-29'),
+(6, 15, 1, 5, '2019-03-29');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `produto`
+-- Table structure for table `produto`
 --
 
-DROP TABLE IF EXISTS `produto`;
-CREATE TABLE IF NOT EXISTS `produto` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `produto` (
+  `id` int(11) NOT NULL,
   `nome` varchar(150) NOT NULL,
   `marca` varchar(150) NOT NULL,
-  `quantidade` varchar(150) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+  `quantidade` varchar(150) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Extraindo dados da tabela `produto`
+-- Dumping data for table `produto`
 --
 
 INSERT INTO `produto` (`id`, `nome`, `marca`, `quantidade`) VALUES
@@ -63,25 +68,68 @@ INSERT INTO `produto` (`id`, `nome`, `marca`, `quantidade`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuario`
+-- Table structure for table `usuario`
 --
 
-DROP TABLE IF EXISTS `usuario`;
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
   `nome` varchar(150) NOT NULL,
   `email` varchar(150) NOT NULL,
-  `senha` varchar(150) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+  `senha` varchar(150) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Extraindo dados da tabela `usuario`
+-- Dumping data for table `usuario`
 --
 
 INSERT INTO `usuario` (`id`, `nome`, `email`, `senha`) VALUES
 (15, 'franciel.abreu', 'franciel@hotmail.com', '123'),
-(17, 'pedro', 'pedro@hotmail.com', '202cb962ac59075b964b07152d234b70');
+(17, 'pedro', 'pedro@hotmail.com', '202cb962ac59075b964b07152d234b70'),
+(18, 'Hallan Abreu', 'Allan22@gmail.com', '202cb962ac59075b964b07152d234b70');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
